f88a12b33f51aebbbd9a441b2844037522454fd6
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-21 12:58:51 -0400

    services to help us to all the things

---
+++ src/services/GoogleMaps.js
@@ -0,0 +1,27 @@
+/**
+ * Created by aclinton on 10/21/16.
+ */
+import GoogleMaps from 'google-maps'
+
+export default class GoogleMapsService {
+    constructor(el = '', map_options = {}) {
+        if(el){
+            this.el = el;
+        }
+
+        if(map_options){
+            this.options = map_options;
+        }
+
+        this.map = null;
+
+        GoogleMaps.KEY = 'AIzaSyAAVbYD7I7G9WBR1vibPL1jNPq5l9kE5cM';
+    }
+
+    load(cb) {
+        GoogleMaps.load(google => {
+            this.map = new google.maps.Map(this.el, this.options);
+            cb(this.map);
+        });
+    }
+}
