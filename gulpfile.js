f3578cfe1da0595bdb2d92bd58c52438863a0e56
Andrew Clinton <ddrew.clinton@gmail.com>
2016-11-09 16:01:03 -0500

    task to build our env file for us

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,24 @@
  * Created by aclinton on 10/20/16.
  */
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+
+gulp.task('build-config', function () {
+    return gulp.src('./env.js')
+        .pipe(gulp.dest('./src/config'));
+});
 
 elixir(function (mix){
-    mix.copy('./bower_components/materialize/fonts', 'public/fonts');
-    mix.copy('./bower_components/font-awesome/fonts', 'public/fonts');
-    mix.copy('public/fonts', 'build/fonts');
+    mix.task('build-config');
+
+    mix.copy('./bower_components/materialize/fonts', './public/fonts');
+    mix.copy('./bower_components/font-awesome/fonts', './public/fonts');
+    mix.copy('./public/fonts', './build/fonts');
 
     mix.sass([
             'app.scss'
         ],
-        './build/css/app.css');
+        './js/css/app.css');
 
-    mix.copy('./build/css/app.css', './public/css/app.css');
+    mix.copy('./public/css/app.css', './build/css/app.css');
 });
