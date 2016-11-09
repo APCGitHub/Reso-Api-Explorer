a55cb9fa1349e22a1f63dcc3e6bbc155596814a1
Andrew Clinton <ddrew.clinton@gmail.com>
2016-11-09 12:26:06 -0500

    access token service

---
+++ src/services/AccessTokenService.js
@@ -0,0 +1,29 @@
+/**
+ * Created by aclinton on 11/2/16.
+ */
+import Vue from 'vue';
+import Promise from 'es6-promise';
+
+export default class AccessTokenService {
+    constructor(server){
+        this.server = server;
+    }
+
+    getToken(client_secret, code) {
+        let url = this.server.token_endpoint;
+
+        return new Promise((resolve, reject) => {
+            Vue.http.post(url, {
+                client_id: this.server.client_id,
+                client_secret: client_secret,
+                redirect_uri: this.server.redirect_uri,
+                code: code,
+                grant_type: 'authorization_code'
+            }).then((res) => {
+                resolve(res);
+            }, (err) => {
+                reject(err);
+            });
+        });
+    }
+}
