775a859a09345d2fbb80f97da7fac6324649451a
Andrew Clinton <ddrew.clinton@gmail.com>
2016-11-09 12:24:24 -0500

    all the routes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,26 @@
 /**
  * Created by aclinton on 10/20/16.
  */
-import VueRouter from 'vue-router';
-import Login from './components/Login.vue';
-import About from './components/About.vue';
-import Explore from './components/Explore.vue';
+import VueRouter from 'vue-router'
+import Login from './components/Login.vue'
+import About from './components/About.vue'
+import Explore from './components/Explore.vue'
+import Servers from './components/server/Index.vue'
+import NewServer from './components/server/Create.vue'
+import ServerEdit from './components/server/Edit.vue'
 
 const routes = [
-    {path: '/', component: About},
+    {path: '/', name: 'home', component: About},
     {path: '/login', component: Login},
-    {path: '/explore', component: Explore}
+    {path: '/servers', name: 'servers.index', component: Servers},
+    {path: '/servers/:id', name: 'servers.edit', component: ServerEdit},
+    {path: '/servers/create', name: 'servers.create', component: NewServer},
+    {path: '/explore/:id', name: 'explore', component: Explore}
 ];
 
 let router = new VueRouter({
-    mode: 'history',
+    linkActiveClass: 'active',
+    /*mode: 'history',*/
     base: __dirname,
     routes
 });
