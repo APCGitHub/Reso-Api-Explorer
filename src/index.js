76f1ddb81fb43e2545d84c684fe510b47836f65e
Andrew Clinton <ddrew.clinton@gmail.com>
2016-11-09 12:24:13 -0500

    bootstrap it all

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import router from './routes'
 import App from './components/App.vue'
-import $ from 'jquery'
-import materialize from 'materialize-css'
-import VeeValidate from 'vee-validate';
+import VeeValidate from 'vee-validate'
 
-window.$ = $;
+window.$ = window.jQuery = require('materialize-css/node_modules/jquery/dist/jquery.js')
+require('materialize-css');
 
 Vue.use(VueRouter)
 Vue.use(VeeValidate);
+Vue.use(VueResource);
 
 new Vue({
     el: '#app',
