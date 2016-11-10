8dc480590f0776ed8a72f575429c131d913bc610
Andrew Clinton <ddrew.clinton@gmail.com>
2016-11-10 16:53:12 -0500

    apparently i don’t understand vue as well as i thought. this bug had to do with something called “reactivity”. it was fixed by using components for each of the inputs though

---
+++ src/components/explorer/OrderbyInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="valign-wrapper">
+        <div class="input-field col s5">
+            <input v-validate data-rules="required" type="text" :id="name" :name="name" v-model="input_data.value">
+            <label class="active" :for="name">$orderby</label>
+            <span class="error red-text darken-2" v-show="errors.has(name)">{{ errors.first(name) }}</span>
+        </div>
+        <div class="input-field col s4">
+            <select class="browser-default" v-model="input_data.direction">
+                <option></option>
+                <option value="asc">asc</option>
+                <option valu="desc">desc</option>
+            </select>
+        </div>
+        <div class="col s3">
+            <button @click="destroy" class="waves-effect waves-light red lighten-2 btn valign right">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script type="text/babel">
+    export default {
+        props: ['data', 'name', 'index', 'eventName'],
+        data() {
+            return {
+                input_data: this.data
+            }
+        },
+        methods: {
+            destroy() {
+                this.$emit(this.eventName, this.index);
+            }
+        }
+    }
+</script>
