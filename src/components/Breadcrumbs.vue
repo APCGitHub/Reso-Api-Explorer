2fbc8345386325391a6b64efe89aada12899b63e
Andrew Clinton <ddrew.clinton@gmail.com>
2016-11-17 09:46:44 -0500

    fill out breadcrumbs view

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="breadcrumb-wrapper">
+        <template v-for="(crumb, index) in crumbs">
+            <router-link v-if="index < crumbs.length - 1" class="breadcrumb" tag="a" :to="crumb.url">{{crumb.name}}</router-link>
+            <a href="javascript:;" class="breadcrumb" v-else>{{crumb.name}}</a>
+        </template>
+    </div>
+</template>
+
+<script type="text/babel">
+    import BreadcrumbService from '../services/BreadcrumbService';
+
+    export default {
+        breadcrumbService: null,
+        data () {
+            return {
+                crumbs: []
+            }
+        },
+        created () {
+            this.breadcrumbService = new BreadcrumbService();
+        },
+        mounted () {
+            this.crumbs = this.breadcrumbService.build(this.$router.currentRoute);
+        },
+        watch: {
+            $route (to, from) {
+                let crumbs = this.breadcrumbService.build(to);
+                this.crumbs = crumbs;
+            }
+        }
+    }
+</script>
