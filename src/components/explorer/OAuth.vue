8ade8f4eb5008639ad93034cb5be63ae612f17d6
Andrew Clinton <ddrew.clinton@gmail.com>
2017-01-12 16:31:18 -0500

    new oauth views

---
+++ src/components/explorer/OAuth.vue
@@ -0,0 +1,118 @@
+<template>
+	<div class="row mt-lg">
+		<div class="col s12 m6 offset-m3">
+			<div class="section">
+				<div class="card">
+					<div class="card-content" v-if="server">
+						<div class="card-title">{{server.name}}</div>
+						<div class="row">
+							<div class="col s12 m6">
+								<button :disabled="code" @click="fetchAuthCode" class="btn waves-effect waves-light cyan lighten-1"><i class="fa fa-check mr-sm" v-show="code"></i>Fetch Code</button>
+							</div>
+							<div class="col s12 m6">
+								<button :disabled="!code || got_access_token" @click="checkIfHasSecret" class="btn waves-effect waves-light cyan lighten-1">
+									<i class="fa fa-check mr-sm" v-show="got_access_token"></i>Fetch Access Token
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script type="text/babel">
+	import ServerService from '../../services/ServerService';
+	import AccessTokenService from '../../services/AccessTokenService';
+	import Server from '../../models/Server';
+	import Promise from 'bluebird';
+	import swal from 'sweetalert';
+
+	export default {
+		serverService: null,
+		accessTokenService: null,
+		data() {
+			return {
+				server: null,
+				code: null,
+				got_access_token: false
+			};
+		},
+		created() {
+			let id = this.$route.params.id;
+
+			this.serverService = new ServerService();
+
+			Server.find(id).then(server => {
+				this.server = server;
+				this.accessTokenService = new AccessTokenService(this.server);
+
+				return Promise.resolve(server);
+			}).then(server => {
+				if(this.$route.query.code){
+					this.code = this.$route.query.code;
+				}
+			}).catch(err => {
+				console.log(err);
+			});
+		},
+		methods: {
+			fetchAuthCode() {
+				let url = ServerService.openIdConnectUrl(this.server);
+
+				window.location = url;
+			},
+			checkIfHasSecret() {
+				if(this.server.client_secret){
+					this.fetchAccessToken(this.server.client_secret);
+				} else {
+					swal({
+                        title: "Credentials",
+                        text: "Please supply your client_secret.",
+                        type: "input",
+                        showCancelButton: true,
+                        closeOnConfirm: false,
+                        animation: "slide-from-top",
+                        inputPlaceholder: "secret"
+                    },
+                    (client_secret) => {
+                    	if (client_secret === false) {
+                            return false;
+                        }
+
+                        if (client_secret === "") {
+                            swal.showInputError("The secret is required!");
+                            return false;
+                        }
+
+                    	this.fetchAccessToken(client_secret);
+                    });
+				}
+			},
+			fetchAccessToken(secret) {
+				this.accessTokenService.getToken(secret, this.code).then(res => {
+                		let body = res.body;
+                		let expires_at = AccessTokenService.setExpiresAt(body.expires_in);
+
+                		return this.server.update({
+                			access_token: body.access_token,
+                        	expires_at: expires_at
+                		});
+					}).then(res => {
+						this.got_access_token = true;
+						swal({
+							title: 'Success!', 
+							text: 'Successfully fetched a new access token.',
+							type:'success',
+							confirmButtonText: 'Continue'
+						}, () => {
+							this.$router.push({name: 'explore', params: {id: this.server.id}});
+						});
+					}).catch(err => {
+						swal('Uh oh', 'There was an error while trying to fetch a new access token', 'warning');
+					});
+			}
+		}
+	}
+</script>
---
+++ src/components/server/components/OpenIdConfig.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="row valign-wrapper">
+	    <div class="col s12 m2">
+	        <label for="'config.openid.' + index + '.name'" class="active">Name</label>
+	        <input id="'config.openid.' + index + '.name'" name="'config.openid.' + index + '.name'" v-model="config.name">
+	    </div>
+	    <div class="col s12 m4 input-field">
+	        <label class="active" for="'config.openid.' + index + '.key'">Key</label>
+	        <input :id="'config.openid.' + index + '.key'" :name="'config.openid.' + index + '.key'" v-model="config.key">
+	    </div>
+	    <div class="col s12 m4 input-field">
+	        <template v-if="!config.no_value">
+	            <label class="active" for="'config.openid.' + index + '.value'">Value</label>
+	            <input :id="'config.openid.' + index + '.value'" :name="'config.openid.' + index + '.value'" v-model="config.value">
+	        </template>
+	        <div v-if="config._key === 'redirect_uri'">
+	        	<input v-model="config.encode" :id="'config.openid.' + index + '.checked'" :name="'config.openid.' + index + '.checked'" type="checkbox" class="filled-in">
+	        	<label :for="'config.openid.' + index + '.checked'">URL Encode</label>
+	        </div>
+	    </div>
+	    <div class="col s12 m2">
+	    	<button @click="remove" v-if="!config.required" type="button" class="waves-effect waves-light thin-button red lighten-2 btn valign left">
+	    		<i class="fa fa-trash"></i>
+	    	</button>
+	    	<button type="button" class="waves-effect waves-light thin-button cyan lighten-2 btn valign right handle">
+	    		<i class="fa fa-bars" aria-hidden="true"></i>
+	    	</button>
+	    </div>
+	</div>
+</template>
+
+<script type="text/babel">
+	export default {
+		props: ['config', 'index'],
+		data() {
+			return {}
+		},
+		methods: {
+			remove() {
+				this.$emit('remove', this.index);
+			}
+		}
+	}
+</script>
